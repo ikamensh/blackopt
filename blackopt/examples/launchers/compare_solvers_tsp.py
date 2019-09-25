0b95b2dfe32df340180f834f58e3e6e8a60b4b00
Ilya <ikamenshchikov@gmail.com>
2019-09-25 17:53:16 +0200

    win friendly!

--- blackopt/examples/launchers/compare_solvers_tsp.py
+++ blackopt/examples/launchers/compare_solvers_tsp.py
@@ -6,12 +6,11 @@ from blackopt.util.document import generate_report
 
 from blackopt.compare import compare_solvers, SolverFactory
 
-n_steps = int(2e5)
-n_trials = 5
+n_steps = int(2e6)
+n_trials = 8
 
 cities = 200
-problem = TspProblem.random_problem(2, cities)
-
+problem = TspProblem.random_problem(15, cities)
 
 solvers = []
 solvers.append(SolverFactory(RandomSearch, problem, TspSolution))
@@ -29,6 +28,9 @@ solvers.append(
     SolverFactory(GeneticAlgorithm, problem, TspSolution, 10, 1 / cities, 1)
 )
 
-
-ms = compare_solvers(n_trials, n_steps, solvers)
-generate_report(problem, ms)
+if __name__ == "__main__":
+    import time
+    t = time.time()
+    ms = compare_solvers(n_trials, n_steps, solvers)
+    generate_report(problem, ms)
+    print(time.time() - t)
