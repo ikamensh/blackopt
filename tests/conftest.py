d5824ccbec0c2e86d174478162d88a807a74d248
ikkamens <ikkamens@amazon.com>
2020-02-14 13:53:09 +0100

    maybe poetry, add folder to sys.path in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,14 @@ from blackopt.examples.problems import TspSolution, TspProblem
 from blackopt.examples.problems import BumpySolution, BumpyProblem
 from blackopt.examples.problems import StepSolution, StepProblem
 
+import sys
+
+import os
+
+cur_dir = os.path.dirname(__file__)
+blackopt_dir = os.path.join(cur_dir, "..", "blackopt")
+sys.path.append(blackopt_dir)
+
 @pytest.fixture()
 def with_tmp_root(tmpdir):
     rootdir = get_rootdir()
